8400efafdad225ee2ec26901b28ee6b3b0d9b9ce
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-04-30 01:49:52 -0400

    created a Playlist class

---
+++ src/list/playlist/Playlist.java
@@ -0,0 +1,19 @@
+package list.playlist;
+
+import list.List;
+
+/*
+ * A Playlist is a collection of Songs. It is a subclass of List. An object
+ * of type List<Song> could be used instead of Playlist. This class is used
+ * for convenience.
+ * */
+public class Playlist extends List {
+
+	/*
+	 * Creates an empty playlist.
+	 * */
+	public Playlist() {
+		super();
+	}
+
+}
